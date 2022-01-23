fe5579df68a127de4bd893626a43a73039863851
tomaszpankowski <pankowski.email@gmail.com>
2022-01-23 14:49:26 +0100

    routing modfied

---
+++ src/views/bouquets.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Bouquets extends Component{
+    render(){
+        return(        
+            <Container fluid className="bouquets-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Bouquets;
