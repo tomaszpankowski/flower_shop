d9972508f1ef2169940c0b79366406e6a187d8d1
tomaszpankowski <pankowski.email@gmail.com>
2022-02-14 08:18:31 +0100

    layout done

--- src/views/bouquets.js
+++ src/views/bouquets.js
@@ -1,11 +1,16 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import BouquetsS1 from "../components/bouquets-s1";
+import BouquetsS2 from "../components/bouquets-s2";
+import BouquetsS3 from "../components/bouquets-s3";
 
 class Bouquets extends Component{
     render(){
         return(        
-            <Container fluid className="bouquets-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-100vh p-0">
+                <BouquetsS1/>
+                <BouquetsS2/>
+                <BouquetsS3/>
             </Container>    
         );
     }
